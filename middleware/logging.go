8ddc934a17d991b73467a49a93fa0bf89a93e597
hannesortmeier <ortmeier.hannes@gmail.com>
2025-11-05 19:51:43 +0100

    fix: Check if devideId length before slicing to 6 characters

--- middleware/logging.go
+++ middleware/logging.go
@@ -11,13 +11,20 @@ import (
 func RequestLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
 		start := time.Now()
+		deviceID := request.Header.Get("X-Device-Id")
+		deviceIDShort := ""
+		if len(deviceID) >= 6 {
+			deviceIDShort = deviceID[:6]
+		} else {
+			deviceIDShort = deviceID
+		}
 
 		// Get logger with request ID from context
 		logger := GetLoggerWithReqID(request.Context()).WithFields(logrus.Fields{
 			"method": request.Method,
 			"uri":    request.RequestURI,
 			// Only log the first 6 characters of the device ID for privacy
-			"deviceId": request.Header.Get("X-Device-Id")[:6],
+			"deviceId": deviceIDShort,
 		})
 
 		logger.Info("Incoming request")
